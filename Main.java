ddf420f268f0dc17c6a2ddbb65a0580056ddc273
sadaliedirisinghe59-afk <sadaliedirisinghe59@gmail.com>
2026-04-25 22:48:37 +0530

    Explain the this code simply
    comments are used to make the code easier to understand and use

--- Main.java
+++ Main.java
@@ -1,32 +1,40 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList; // use Array list
+import java.util.Scanner;  // get user input
 
+// represent the warehouse iteam
 class Item {
     int id;
     String name;
     int quantity;
     double price;
-
+    
+// Constructor - initializes item details when object is created
     Item(int id, String name, int quantity, double price) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
         this.price = price;
     }
-
+    
+// Method to display item details
     void display() {
         System.out.println("ID: " + id + ", Name: " + name +
                 ", Quantity: " + quantity + ", Price: " + price);
     }
 }
 
+// Main class
 public class Main {
+     // ArrayList to store all inventory items
     static ArrayList<Item> inventory = new ArrayList<>();
+    
+      // Scanner object to read user input
     static Scanner scanner = new Scanner(System.in);
+    
 
     public static void main(String[] args) {
         int choice;
-
+// Loop runs until user chooses to exit
         do {
             System.out.println("\n--- Warehouse Inventory System ---");
             System.out.println("1. Add Item");
@@ -36,20 +44,21 @@ public class Main {
             System.out.println("5. Exit");
             System.out.print("Enter choice: ");
 
-            choice = scanner.nextInt();
-
+            choice = scanner.nextInt();// Read user choice
+            
+  // Perform action based on user choice
             switch (choice) {
                 case 1:
-                    addItem();
+                    addItem(); // Add new item
                     break;
                 case 2:
-                    viewItems();
+                    viewItems();// Display all items
                     break;
                 case 3:
-                    updateItem();
+                    updateItem();// Update existing item
                     break;
                 case 4:
-                    deleteItem();
+                    deleteItem(); // Delete item
                     break;
                 case 5:
                     System.out.println("Exiting...");
@@ -60,7 +69,8 @@ public class Main {
 
         } while (choice != 5);
     }
-
+    
+   // Method to add a new item to the inventory
     static void addItem() {
         System.out.print("Enter ID: ");
         int id = scanner.nextInt();
@@ -74,27 +84,32 @@ public class Main {
 
         System.out.print("Enter Price: ");
         double price = scanner.nextDouble();
-
+        
+  // Add new item to the list
         inventory.add(new Item(id, name, quantity, price));
         System.out.println("Item added successfully!");
     }
-
+    
+// Method to display all items
     static void viewItems() {
         if (inventory.isEmpty()) {
             System.out.println("No items in inventory.");
             return;
         }
-
+        
+          
         System.out.println("\n--- Inventory List ---");
         for (Item item : inventory) {
             item.display();
         }
     }
-
+    
+// Loop through each item and display it
     static void updateItem() {
         System.out.print("Enter ID to update: ");
         int id = scanner.nextInt();
-
+        
+ // Search for item by ID
         for (Item item : inventory) {
             if (item.id == id) {
                 scanner.nextLine(); 
@@ -107,7 +122,8 @@ public class Main {
 
                 System.out.print("Enter new price: ");
                 item.price = scanner.nextDouble();
-
+                
+// If item not found
                 System.out.println("Item updated successfully!");
                 return;
             }
@@ -115,11 +131,14 @@ public class Main {
 
         System.out.println("Item not found!");
     }
-
+    
+// Method to delete an item
     static void deleteItem() {
         System.out.print("Enter ID to delete: ");
         int id = scanner.nextInt();
-
+        
+ // Search for item and remove it
+        
         for (Item item : inventory) {
             if (item.id == id) {
                 inventory.remove(item);
